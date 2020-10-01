c06d813293ad323230addeed6bea11c0e277f8a5
Maycon Carlete <maycon.carlete@hotmail.com>
2020-10-01 16:27:07 -0300

    feat: add exercise 1 to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 import 'module-alias/register'
+import { FaceDetector } from '@src/ex1/ex1'
+
+const faceDetector = new FaceDetector()
+const countSmiles = faceDetector.countSmiles([';D', ':-(', ':-)', ';~)'])
+console.log(`Result exercise1: ${countSmiles}`)
